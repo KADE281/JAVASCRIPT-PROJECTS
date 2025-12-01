8cf384bd8a843ec681ee2eba611a9bb2a365c01f
KADE <kwizeradev1@gmail.com>
2025-12-01 21:00:06 +0200

    A little bit of the changes on this BroCode Folder

---
+++ BroCodeJs/valiEmail.js
@@ -0,0 +1,14 @@
+
+function isValidEmail(){
+    if(email.includes("@")){
+        return true;
+
+    }
+    else{
+        return false;
+    }
+
+}
+let result=isValidEmail();
+
+console.log(`Is ${email} a valid email?: `,result);
